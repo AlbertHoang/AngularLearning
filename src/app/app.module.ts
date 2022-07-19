d10ef884ac905dcb2b9f90c1125901912b7ddb71
AlbertHoang <alberthoang.au@gmail.com>
2022-07-19 17:40:11 +1000

    Handle Text Input & Event Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordComponent
+    WordComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
