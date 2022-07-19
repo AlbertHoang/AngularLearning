1e1a21d0a18e3a244d5831c5f8d41045eda93196
AlbertHoang <alberthoang.au@gmail.com>
2022-07-19 17:22:57 +1000

    Event Binding

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -11,8 +11,13 @@ export class WordComponent implements OnInit {
   vn = "Xin chao";
   imageURL = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
   constructor() { }
+  forgot = false;
 
   ngOnInit(): void {
+  }
+
+  toggleForgot() {
+    this.forgot = !this.forgot;
   }
 
 }
