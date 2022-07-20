6ecd66dc71f15a738316d7ed28c8ee5a6942f1b5
AlbertHoang <alberthoang.au@gmail.com>
2022-07-21 00:11:58 +1000

    Structural Drirectives ngIF ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { FormsModule } from '@angular/forms';
+import { StructComponent } from './struct/struct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
-    UserFormComponent
+    UserFormComponent,
+    StructComponent
   ],
   imports: [
     BrowserModule,
