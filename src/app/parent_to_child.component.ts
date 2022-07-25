6032b77796e1976c83f75019228ec637c3832358
AlbertHoang <alberthoang.au@gmail.com>
2022-07-25 18:42:52 +1000

    Viewchild mychild:any increase child value

--- src/app/parent_to_child.component.ts
+++ src/app/parent_to_child.component.ts
@@ -1,14 +1,20 @@
-import {Component} from '@angular/core'
+import {Component, ViewChild} from '@angular/core'
+import { Child1Component } from './child_1.component'
 
 @Component({
     selector: 'app-parent-to-child',
     template: `
     <h3>Parent to child component!</h3>
-    <button>Add to child 1</button>
+    <button (click)="onAddForChild()">Add to child 1</button>
     <app-child-1></app-child-1>
     `
 })
 
 export class ParentToChildComponent {
 
+    @ViewChild(Child1Component) myChild: any;
+
+    onAddForChild() {
+        this.myChild.value++;
+    }
 }
