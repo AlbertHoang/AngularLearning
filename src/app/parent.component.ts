d20253f02b8432a7ee906a86b628f75fddb678d4
AlbertHoang <alberthoang.au@gmail.com>
2022-07-25 16:22:32 +1000

    Child component increase element in parent component

--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -2,11 +2,10 @@ import  {Component} from '@angular/core';
 
 @Component({
     selector: 'app-parent',
-    template: `<h3>
-        {{value}}
-        <br>
-        <app-child></app-child>
-        </h3>`
+    template: `
+        <h3>{{ value }}</h3>
+        <app-child (myClick)="value = value + 1"></app-child>
+        `
 })
 
 export class ParentComponent {
