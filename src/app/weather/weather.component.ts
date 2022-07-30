55a82c184664cdc6c710fa6b932e1e15c5643446
AlbertHoang <alberthoang.au@gmail.com>
2022-07-31 00:00:14 +1000

    Handling error

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,29 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { WeatherService } from './weather.service'
+import { WeatherService } from './weather.service';
 
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css'],
-  providers: [WeatherService]
+    selector: 'app-weather',
+    templateUrl: './weather.component.html',
+    styleUrls: ['./weather.component.css'],
+    providers: [WeatherService]
 })
 export class WeatherComponent implements OnInit {
 
-  txtCityName = '';
-  cityName = '';
-  temp = null;
+    txtCityName = '';
+    cityName = '';
+    temp = null;
+    isLoading = false;
 
-  constructor(private weatherService: WeatherService) {}
+    constructor(private weatherService: WeatherService) { }
 
-  ngOnInit(): void {}
+    ngOnInit(): void { }
 
-  getWeather(){
-    this.weatherService.getTemp(this.txtCityName)
-    .subscribe(data => {
-        // console.log(data.main.temp));
-        this.cityName = this.txtCityName;
-        this.temp = data.main.temp;
-    })
-  }
+    getWeather() {
+        this.isLoading = true;
+        this.weatherService.getTemp(this.txtCityName)
+            .subscribe(
+                data => {
+                    // console.log(data.main.temp));
+                    this.cityName = this.txtCityName;
+                    this.temp = data.main.temp;
+                    this.isLoading = false;
+                }, error => {
+                    console.log(error);
+                    alert('Cannot find your city!');
+                    this.isLoading = false;
+                    this.cityName = '';
+                    this.txtCityName = '';
+                }
+            );
+    }
+
+    getMessage() {
+        if(this.isLoading){
+            return 'Loading...'
+        }
+        return this.cityName === '' ? "Enter your city name" : (this.cityName + ' is now ' + this.temp);
+    }
 
 }
