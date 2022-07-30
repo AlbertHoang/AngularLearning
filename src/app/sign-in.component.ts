bb4b9740ce84c8cc1b6de1bd34a9c78b2e3d8ba2
AlbertHoang <alberthoang.au@gmail.com>
2022-07-31 01:08:27 +1000

    ngForm and template reference variable

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -3,13 +3,22 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-sign-in',
     template: `
-        <h3>Sign In Component</h3>
-        <input placeholder="Email">
-        <br><br>
-        <input type="password" placeholder="Password">
-        <br><br>
-        <button>Submit</button>
+        <form (submit)="onSubmit(formSignIn);" #formSignIn="ngForm">
+            <h3>Sign In Component</h3>
+            <input placeholder="Email" [(ngModel)]="email" name="email">
+            <br><br>
+            <input type="password" placeholder="Password" [(ngModel)]="password" name="password">
+            <br><br>
+            <button>Submit</button>
+        <form>
     `
 })
 
-export class SignInComponent {}
+export class SignInComponent {
+    email = '';
+    password = '';
+    onSubmit(formSignIn:any){
+        console.log(formSignIn.value);
+
+    }
+}
