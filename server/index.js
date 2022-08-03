d3e04ae6d56f33f008d4d4e54d17f3a40562a86c
AlbertHoang <alberthoang.au@gmail.com>
2022-08-03 17:10:06 +1000

    Fix localhost:4200 & 3000 | Cross-Origin Resource Sharing | Access-Control-Allow-Origin on server | index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+    next();
+});
+
 app.get('/', (req,res)=> res.send('Hello'));
 
 app.post('/signin', (req, res)=>{
