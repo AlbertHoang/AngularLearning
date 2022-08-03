4e98b4f3b58a2df5f05f81520434d01d78475c4f
AlbertHoang <alberthoang.au@gmail.com>
2022-08-03 14:13:32 +1000

    Form sign-in.component| ngModelGroup | Validator MinLength Pattern

--- src/app/sign-in.component.ts
+++ src/app/sign-in.component.ts
@@ -9,11 +9,24 @@ import { Component } from "@angular/core";
             <p *ngIf="txtEmail.touched && txtEmail.errors?.['required']">Email is required</p>
             <p *ngIf="txtEmail.touched && txtEmail.errors?.['email']">Email is not valid</p>
             <br><br>
-            <input type="password" placeholder="Password" ngModel name="password">
+            <input type="password"
+            placeholder="Password"
+            ngModel
+            name="password"
+            #txtPassword="ngModel"
+            minlength="6"
+            pattern="[a-z]*"
+            >
             <br><br>
+            <div ngModelGroup="subjects">
+            <label><input type="checkbox" [ngModel]="false" name="NodeJS">NodeJS</label>
+            <label><input type="checkbox" [ngModel]="false" name="Angular">Angular</label>
+            <label><input type="checkbox" [ngModel]="false" name="ReactJS">ReactJS</label>
+            </div>
             <button [disabled]="formSignIn.invalid">Submit</button>
         <form>
-
+        <p>{{ txtPassword.errors | json }}</p>
+        <p>{{ formSignIn.value | json }}</p>
     `
 })
 
